118dbd1a22ff751cf0abee6e6c4e3272b1cd0dd9
Yusuf Ansari <yusssuf0786@gmail.com>
2024-05-23 20:45:59 +0530

    store.jsx file redux devtools extension fixed

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,7 +1,6 @@
 import { createStore } from "redux";
 import rootReducer from "./rootReducer";
-import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer, composeWithDevTools())
+const store = createStore(rootReducer)
 
 export default store
