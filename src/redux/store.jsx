ab7ddb08c044f0ddd16482163804b56439fa3417
Yusuf Ansari <yusssuf0786@gmail.com>
2024-06-06 18:12:04 +0530

    reinstalled redux

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,6 +1,6 @@
 import { applyMiddleware, createStore } from "redux";
-import rootReducer from "./rootReducer";
 import { thunk } from "redux-thunk";
+import rootReducer from "./rootReducer";
 
 const store = createStore(rootReducer, applyMiddleware(thunk))
 
